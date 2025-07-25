ee4fe85d8086fc0f2b8b88ba2723465e95fc7a0a
viraj upadhyay <vsu1833@gmail.com>
2025-07-25 23:32:46 +0530

    Kth Missing Positive Number

---
+++ kth_missing_pos.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        int findKthPositive(vector<int>& arr, int k) {
+            for (int i = 0; i < arr.size(); ++i) {
+                if (arr[i] <= k) {
+                    k++;
+                } else {
+                    break;
+                }
+            }
+            return k;
+        }
+    };
+    
+int main() {
+    Solution solution;
+    vector<int> arr = {2, 3, 4, 7, 11};
+    int k = 5;
+    cout << "The kth missing positive number is: " << solution.findKthPositive(arr, k) << endl;
+    return 0;
+}
