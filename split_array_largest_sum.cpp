fe6283d6612b66abb0d5b666d6ff15964451b638
viraj upadhyay <vsu1833@gmail.com>
2025-07-27 23:28:24 +0530

    Split Array - Largest Sum

---
+++ split_array_largest_sum.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            subarraySum += a[i];
+        }
+        else {
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+
+int largestSubarraySumMinimized(vector<int> &a, int k) {
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+
+    for (int maxSum = low; maxSum <= high; maxSum++) {
+        if (countPartitions(a, maxSum) == k)
+            return maxSum;
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> a = {10, 20, 30, 40};
+    int k = 2;
+    int ans = largestSubarraySumMinimized(a, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+
