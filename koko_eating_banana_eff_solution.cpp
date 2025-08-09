041bb551af9ac5b35a796b13bfc8524a468c54f7
viraj upadhyay <vsu1833@gmail.com>
2025-08-10 00:10:10 +0530

    koko eating banananas solutin using binary search

---
+++ koko_eating_banana_eff_solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMax(vector<int> &v) {
+    int maxi = INT_MIN;
+    int n = v.size();
+    for (int i = 0; i < n; i++) {
+        maxi = max(maxi, v[i]);
+    }
+    return maxi;
+}
+
+int calculateTotalHours(vector<int> &v, int hourly) {
+    int totalH = 0;
+    int n = v.size();
+    for (int i = 0; i < n; i++) {
+        totalH += ceil((double)(v[i]) / (double)(hourly));
+    }
+    return totalH;
+}
+
+int minimumRateToEatBananas(vector<int> v, int h) {
+    int low = 1, high = findMax(v);
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int totalH = calculateTotalHours(v, mid);
+        if (totalH <= h) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6};
+    int h = 4;
+    int ans = minimumRateToEatBananas(v, h);
+    cout << "Koko should eat atleast " << ans << " bananas/hr.\n";
+    return 0;
+}
