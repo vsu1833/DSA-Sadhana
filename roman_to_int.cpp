7c7e6d91b9749a4b025238f3b6c92d02aed262a3
viraj upadhyay <vsu1833@gmail.com>
2025-08-24 00:27:08 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ roman_to_int.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        unordered_map<char, int> roman = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int result = 0;
+        for (int i = 0; i < (int)s.size(); i++)
+        {
+            if (i + 1 < (int)s.size() && roman[s[i]] < roman[s[i + 1]])
+            {
+                result -= roman[s[i]];
+            }
+            else
+            {
+                result += roman[s[i]];
+            }
+        }
+        return result;
+    }
+};
+
+#ifndef ONLINE_JUDGE
+int main()
+{
+    Solution sol;
+    string s;
+    cout << "Enter Roman numeral: ";
+    if (cin >> s)
+    {
+        cout << sol.romanToInt(s) << "\n";
+    }
+    return 0;
+}
+#endif
